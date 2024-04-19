f7f6a156aa57f9689e806ea1758ec8a8f4c1f50f
Mahi_G13 <maicolgallego14@gmail.com>
2024-04-18 21:40:36 -0500

    add first commit

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-/* comentario */
-/* otro comment */
+let userName = prompt("Ingresa tu nombre y tu apellido").toLowerCase();
+var splitedArray = userName.split(" ");
+console.log(splitedArray)
+splitedArray.forEach(Element => {
+    console.log(Element.slice(0,3))
+    return Element.slice(0,3)
+});
+
