148eb40c1a492469760cd4cab9dc50a815d8df41
Jeronimo <jerocruz76@gmail.com>
2024-04-23 21:23:40 -0500

    add few changes to challenge 3

--- seccion1/reto3.js
+++ seccion1/reto3.js
@@ -1,25 +1,14 @@
-// Validador de correos electronicos
+const email = prompt("Ingresa tu email")
+listaEmail = []
+listaEmail.push(email)
+validarEmail()
 
-const email = prompt("Por favor ingresa tu correo electronico: ");
+function validarEmail(){
+    if(listaEmail.includes('@.')|| listaEmail.includes('.@')||listaEmail.includes(" ")){
+        alert("Correo inválido")}
 
-const checkHasGotAt = (el) => {
-    return el.includes("@");
 }
 
-const checkHasGotDotRightAfterAt = (el) => {
-    // first check if it has an @
-    if (!checkHasGotAt(el)) return false;
-    // then check if it has a dot right after the @
-    return el.split("@")[1].includes(".");
-}
-
-const checkAtAndDotAreNothTogether = (el) => {
-    // first check if it has an @
-    if (!checkHasGotAt(el)) return false;
-    // then check if it has a dot right after the @
-    return !el.includes("@."); // if it includes @. it means they are together
-}
-
-const checkNotEmptySpaces = (el) => {
-    return !el.includes(" "); // if it includes a space, it means it has empty spaces
+function validarArroba(){
+    return(listaEmail.includes('@'))
 }
