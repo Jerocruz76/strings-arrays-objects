696cd1c13144b66651218d577bf9dcf8fbc2a7f5
Jeronimo <jerocruz76@gmail.com>
2024-04-26 21:08:51 -0500

    add first challenge of the second section

--- seccion2/reto2.js
+++ seccion2/reto2.js
@@ -1,9 +1,9 @@
 
-let notas = prompt("Ingresa las notas del 1 al 100 separadas por comas:")
+let notas = prompt("Ingresa las notas de tus estudiantes del 1 al 100")
 
 let arrayNotas = notas.split(",")
 arrayNotas = arrayNotas.map(num => parseInt(num))
 
 const promedio = arrayNotas.reduce((e,n) => e+n/arrayNotas.length, 0)
 
-alert(`El promedio de las notas es ${promedio}`)
+Math.max(arrayNotas)
